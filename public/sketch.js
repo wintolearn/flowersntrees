0c49a14adb010217fa57151195e9ba160250640d
Jon Martin <jonathanmartinnyc@gmail.com>
2020-03-14 20:28:43 -0400

    Initial commit

---
+++ public/sketch.js
@@ -0,0 +1,108 @@
+// ITP Networked Media, Fall 2014
+// https://github.com/shiffman/itp-networked-media
+// Daniel Shiffman
+
+// Keep track of our socket connection
+var socket;
+var codeString;
+var input;
+var codeToSend = "";
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+
+    input = createInput();
+    input.position(20, 65);
+
+    button = createButton('submit');
+    button.position(input.x + input.width, 65);
+    button.mousePressed(updateCode);
+
+  // Start a socket connection to the server
+  // Some day we would run this server somewhere else
+  socket = io.connect('http://localhost:3000');
+  // We make a named event called 'mouse' and write an
+  // anonymous callback function
+  socket.on('mouse',
+    // When we receive data
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+      // Draw a blue circle
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
+  );
+
+  socket.on('codeString',
+        // When we receive data
+        function(data) {
+            //console.log("Got: " + data);
+            codeString = data;
+
+            // Draw a blue circle
+            fill(0,0,255);
+            eval(codeString);
+            //noStroke();
+            //ellipse(data.x, data.y, 20, 20);
+        }
+    );
+}
+
+function draw() {
+  // Nothing
+    sendCheckUpdates();
+    //eval(codeString);
+}
+
+function mouseDragged() {
+  // Draw some white circles
+  fill(255);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+  // Send the mouse coordinates
+  sendmouse(mouseX,mouseY);
+}
+
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse',data);
+}
+
+// Function to check for updates
+function sendCheckUpdates() {
+    // We are sending!
+
+
+    var data = codeToSend;
+
+    console.log("check updates "+codeToSend);
+
+    // Send that object to the socket
+    socket.emit('checkUpdates',data);
+}
+
+// Function to append to text file
+function updateCode() {
+    codeToSend = input.value();
+    console.log(codeToSend);
+    // We are sending!
+    //console.log("send code to append");
+    //var data = "";
+
+    // Send that object to the socket
+    //socket.emit('checkUpdates',data);
+    //socket.emit('Append',data);
+
+}
---
+++ server.js
@@ -0,0 +1,101 @@
+// Based off of Shawn Van Every's Live Web
+// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+
+// Using express: http://expressjs.com/
+var express = require('express');
+// Create the app
+var app = express();
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT || 3000, listen);
+
+var codeString;
+
+
+
+// This call back just tells us that the server has started
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+fs = require('fs');
+
+/*
+fs.writeFile('public/helloworld.txt', 'Hello my friends!', function (err) {
+    if (err) return console.log(err);
+    console.log('Hello World > helloworld.txt');
+});
+*/
+
+fs.readFile('public/helloworld.txt','utf8', function (err,data) {
+    if (err) return console.log(err);
+    codeString = data;
+    console.log(codeString);
+});
+
+
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function (socket) {
+
+    console.log("We have a new client: " + socket.id);
+
+    // Let's try to send a code string
+      io.sockets.emit('codeString', codeString);
+
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+
+        socket.on('checkUpdates',
+          function(data) {
+              // Data comes in as whatever was sent, including objects
+              console.log("Received: checkUpdates");
+
+              if(data !== "") {
+                  fs.appendFile('public/helloworld.txt', data, function (err) {
+                      if (err) return console.log(err);
+                      //codeString = data;
+                      //console.log(codeString);
+                  });
+              }
+
+              fs.readFile('public/helloworld.txt','utf8', function (err,data) {
+                  if (err) return console.log(err);
+                  codeString = data;
+                  //console.log(codeString);
+              });
+              io.sockets.emit('codeString', codeString);
+
+
+          }
+      );
+
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
+  }
+);
